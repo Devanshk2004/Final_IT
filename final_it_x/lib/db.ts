03591e05eb7aaa35e5cf46590ec7d17b80cbe88a
Devanshk2004 <134846636+Devanshk2004@users.noreply.github.com>
2025-06-10 22:54:18 +0530

    mongo4

--- final_it_x/lib/db.ts
+++ final_it_x/lib/db.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, {Mongoose} from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
@@ -6,4 +6,13 @@ if (!MONGODB_URI) {
     throw new Error("MONGODB_URI IS NOT DEFINED");
 }
 
+declare global {
+    // eslint-disable-next-line no-var
+    var mongoose: { conn: Mongoose | null; promise: Promise<Mongoose> | null } | undefined;
+  }
 
+let cached = global.mongoose;
+
+if (!cached){
+    cached = global.mongoose = { conn : null, promise: null}
+}
